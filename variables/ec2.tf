b733909451ce35f8fb25f77c6f53c41d8bf513c3
NarendraReddy <narendra.nya@gmail.com>
2024-05-12 14:14:54 -0700

    AM

---
+++ variables/ec2.tf
@@ -0,0 +1,36 @@
+#ec2 instance
+resource "aws_instance" "db"{
+  ami                     = var.image_id
+  instance_type           = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow_SSH.id] #list #you can add any SG groups. 
+  #vpc_security_group_ids = var.sg_id
+    tags = var.tags
+
+}
+
+
+#security group 
+resource "aws_security_group" "allow_SSH" {
+  name        = var.sg_name
+  description = var.sg_description
+  #vpc_id      = aws_vpc.main.id  #Default VPC we are using. default VPC region.
+  
+
+  tags = var.sg_tags
+#block -- ingress and outgress are block
+    ingress {
+    from_port        = var.sg_port #SSH
+    to_port          = var.sg_port #SSH
+    protocol         = var.sg_protocol
+    cidr_blocks      = var.cidr_allow #allow from everything
+
+  }
+
+    egress {
+    from_port        = 0 #ll ports
+    to_port          = 0
+    protocol         = "-1" #all protocols
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+}
---
+++ variables/variables.tf
@@ -0,0 +1,82 @@
+# EC2 insstance info 
+variable "image_id" {
+    type=string 
+    #default = "ami-090252cbe067a9e58" #if commented out,  It will ask for default value
+    default = "ami-090252cbe067a9e58"
+    description = "RHEL-9 AMI ID"
+}
+
+variable "instance_type" {
+    type=string 
+    default = "t3.micro"
+  
+}
+
+variable "tags" {
+    type=map
+    default = {
+        Project="expense"
+        Module="db"
+        Environment="Dev"
+        Name = "db"
+    }
+  
+}
+
+# variable "sg_id" {
+#     default = [sg-035bd444d13fbad2f]
+  
+# }
+
+variable "sg_name" {
+    default = "allow_SSH"
+  
+}
+
+variable "sg_description" {
+  default = "Allow SSH inbound traffic and all outbound traffic"
+}
+
+variable "sg_tags" {
+    type = map 
+    default = {
+        Name = "allow_SSH"
+        CreatedBy="NNR"
+    }
+  
+}
+
+variable "sg_port" {
+    type = number
+    default = 22 
+  
+}
+
+variable "sg_protocol" {
+  default = "tcp"
+}
+
+variable "cidr_allow" {
+    type = list(string)
+  default = ["0.0.0.0/0"]
+}
+
+# name        = "allow_SSH"
+#   description = "Allow SSH inbound traffic and all outbound traffic"
+#   #vpc_id      = aws_vpc.main.id  #Default VPC we are using. default VPC region.
+  
+
+#   tags = {
+#     Name = "allow_SSH"
+#     CreatedBy="NNR"
+#   }
+
+#     ingress {
+#     from_port        = 22 #SSH
+#     to_port          = 22 #SSH
+#     protocol         = "tcp"
+#     cidr_blocks      = ["0.0.0.0/0"] #allow from everything
+
+#   }
+
+
